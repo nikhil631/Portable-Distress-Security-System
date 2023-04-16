e92ff5077f8ce7a07bc94299d3b94c77e4bac1e7
nikhil631 <nikhiltomar931@gmail.com>
2023-04-16 19:24:59 +0530

    Updated

--- joe/Worker/worker/worker/settings.py
+++ joe/Worker/worker/worker/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-config("DJANGO_SECRET_KEY",default="0$tl@090q%v&^0*(d9x73w7@s@ftv-kh-q=z!=u-gg16zn3dci")
+SECRET_KEY=config("DJANGO_SECRET_KEY",default="0$tl@090q%v&^0*(d9x73w7@s@ftv-kh-q=z!=u-gg16zn3dci")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
