682545f7751c41b45626c4261f4c4bf32bff978d
nikhil631 <nikhiltomar931@gmail.com>
2023-04-28 01:34:00 +0530

    imporved datetime

--- joe/Portable Distress System/security/api/views.py
+++ joe/Portable Distress System/security/api/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from rest_framework.decorators import api_view,authentication_classes,permission_classes
 from rest_framework.response import Response
 from rest_framework.authentication import TokenAuthentication
@@ -6,11 +5,13 @@ from rest_framework.permissions import IsAuthenticated
 import datetime 
 from security.miscellaneous import *
 from django.conf import settings
-# Create your views here.
+
+
 @api_view(["POST"])
 @authentication_classes([TokenAuthentication])
 @permission_classes([IsAuthenticated])
-def api_add_data(request,coor_x,coor_y,emerg,datetime=datetime.datetime.now()):
+def api_add_data(request,coor_x,coor_y,emerg):
+    datetimes=datetime.datetime.now()
     obj=object_filter({'roll_id':request.user.id},'relation_users')
     context={
             "first_name":request.user.first_name,
@@ -18,14 +19,14 @@ def api_add_data(request,coor_x,coor_y,emerg,datetime=datetime.datetime.now()):
             "coordinate_x":coor_x,
             "coordinate_y":coor_y,
             "emergency":emerg,
-            "date":format(datetime,"%d-%m-%Y"),
-            "time":format(datetime,'%I:%M %p'),
+            "date":format(datetimes,"%d-%m-%Y"),
+            "time":format(datetimes,'%I:%M %p'),
             "id":request.user.id,
             "email":request.user.email,
             "phone_relations":[x.relation.phone for x in obj],
             "emails_relations":[x.relation.email for x in obj],
         }
-    # cache_object_queue_add(settings.REDIS_WORKER_QUEUE,context)
-    # object_creator(factor={"roll_id":request.user.id,"coordinate_x":coor_x,"coordinate_y":coor_y,"emergency":emerg,"date_time":datetime},model="incoming_info")
-    # cache_object_delete(f"user_home:{context['id']}")
+    cache_object_queue_add(settings.REDIS_WORKER_QUEUE,context)
+    object_creator(factor={"roll_id":request.user.id,"coordinate_x":coor_x,"coordinate_y":coor_y,"emergency":emerg,"date_time":datetimes},model="incoming_info")
+    cache_object_delete(f"user_home:{context['id']}")
     return Response(context,status=200)
